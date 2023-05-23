9b6ea6b8dea9530fec55728de0c2b8679760fe2a
Jackson Ricardo Wippel <jrwippel@hotmail.com>
2023-05-23 13:20:49 -0300

    Created Project Micro Services

---
+++ src/test/java/com/jrwsolucoesti/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrwsolucoesti.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
